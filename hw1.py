ea7ec3216ab0e31b3d2cf3e5c77491b5d0af59b8
leorasc <31726486+leorasc@users.noreply.github.com>
2022-11-03 00:55:11 +0200

    modified:   hw1-notebook.ipynb 	modified:   hw1.py

--- hw1.py
+++ hw1.py
@@ -71,7 +71,7 @@ def breadth_first_search(start: Any, end: Any, neighbor_function: Callable):
         (0, 0, 0)(1, 0, 0)(2, 0, 0)(2, 0, -1)
         >>> print(breadth_first_search(start=(0,0,0), end=(2,2,-2), neighbor_function= eight_neighbor_function))
         (0, 0, 0)(1, 0, 0)(2, 0, 0)(2, 1, 0)(2, 2, 0)(2, 2, -1)(2, 2, -2)
-        """
+    """
 
     q = deque()
     visited = set()
@@ -87,6 +87,109 @@ def breadth_first_search(start: Any, end: Any, neighbor_function: Callable):
                 # print(w)
                 visited.add(w) #label w as explored
                 q.append((w, v[1]+str(w)))
+
+
+import collections
+
+def print_sort_set(x: set):
+    s = ""
+    while len(x) > 0:
+        y = find_min(x)
+        x.remove(y)
+        if type(y) is type([]):
+            s += print_sort_list(y)
+        elif type(y) is type({}):
+            s += print_sort_dict(y)
+        elif type(y) is type(set()):
+            s += print_sort_set(y)
+        elif type(y) is type(tuple()):
+            s += print_sort_tuple(y)
+        elif type(y) is type(chr(0)):
+            s += '"' + y + '"'
+        else:
+            s += str(y)
+    return s
+
+def find_min(x: set):
+    y = x[0]
+    for z in x:
+        if str(z) < str(y):
+            y = z
+    return y
+
+def print_sort_dict(x: dict):
+    keylist = sorted(x.keys(), key = str) 
+    s = "{"      
+    for i in keylist:
+        t = '"' if type(i) is type(chr(0)) else ""
+        t = "'" if type(i) is type(str) else t
+        if type(x[i]) is type([]):
+            s += t + str(i) + t + ": " + print_sort_list(x[i]) + ", "
+        elif type(x[i]) is type({}):
+            s += t + str(i) + t + ": " + print_sort_dict(x[i]) + ", "
+        elif type(x[i]) is type(set()):
+            s += t + str(i) + t + ": " + print_sort_set(x[i]) + ", "
+        elif type(x[i]) is type(tuple()):
+            s += t + str(i) + t + ": " + print_sort_tuple(x[i]) + ", "
+        elif type(x[i]) is type(chr(0)):
+            s += t + str(i) + t + ': "' + str(x[i]) + '", '
+        elif type(x[i]) is type(str):
+            s += t + str(i) + t + ": '" + str(x[i]) + "', "
+        else:
+            s += t + str(i) + t + ": " + str(x[i]) + ", "
+    s = s if len(s) == 1 else s[:len(s)-2] + "}"
+    return s
+
+def print_sort_tuple(x: tuple):
+    return print_sort_list(sorted(x, key= str))
+
+def print_sort_list(x: list):
+    x.sort(key=str)
+    s = "["
+    for i in x:
+        if type(i) is type([]):
+            s += print_sort_list(i) + ", "
+        elif type(i) is type({}):
+            s += print_sort_dict(i) + ", "
+        elif type(i) is type(set()):
+            s += print_sort_set(i) + ", "
+        elif type(i) is type(tuple()):
+            s += print_sort_tuple(i) + ", "
+        elif type(i) is type(chr(0)):
+            s += '"' + str(i) + '"'
+        elif type(i) is type(str):
+            s += "'" + str(i) + "'"
+        else:
+            s += str(i) + ", "
+    s = s if len(s) == 1 else s[:len(s)-2] + "]"
+    return s
+
+def print_sorted(x):
+    """ Prints the structure of x while it is sorted
+    >>> print_sorted({"a": 5, "c": 6, "b": [1, 3, 2, 4]}) 
+    {"a": 5, "b": [1, 2, 3, 4], "c": 6}
+    >>> print_sorted({"z": 5, "d": 6, "b": [1, 3, 2, 4]})
+    {"b": [1, 2, 3, 4], "d": 6, "z": 5}
+    >>> print_sorted({"x": 5, "c": 6, "a": [6, 3, 2, 4]})
+    {"a": [2, 3, 4, 6], "c": 6, "x": 5}
+    """
+
+    s = ""
+    if type(x) is type([]):
+        s += print_sort_list(x)
+    elif type(x) is type({}):
+        s += print_sort_dict(x)
+    elif type(x) is type(set()):
+        s += print_sort_set(x)
+    elif type(x) is type(tuple()):
+        s += print_sort_tuple(x)
+    elif type(x) is type(chr(0)):
+        s += '"' + str(x) + '"'
+    elif type(x) is type(str):
+        s += "'" + str(x) + "'"
+    else:
+        s += str(x)
+    print(s)
 
 
 
